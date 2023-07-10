d792a4422c4560c766c55a15830419aa03170881
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-09 23:17:11 -0400

    commit tree d9fec21c609bf687f1ea434d3f0f5e2c7312c1fb

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,21 @@
 # your code here
+class Stack
+    def initialize(enumerable = [])
+        @stack = []
+        enumerable.each do |value|
+            @stack.push(value)
+        end        
+    end
+
+    def push(value)
+        @stack.push(value)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[@stack.length - 1]
+    end
+end
